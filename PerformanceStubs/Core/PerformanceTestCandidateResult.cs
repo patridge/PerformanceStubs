6f2c30fa3e7961b413f3b439014881fdadb582c9
Adam Patridge <patridge@gmail.com>
2014-12-12 10:06:39 -0700

    Extract out some metrics data as properties.

--- PerformanceStubs/Core/PerformanceTestCandidateResult.cs
+++ PerformanceStubs/Core/PerformanceTestCandidateResult.cs
@@ -1,9 +1,18 @@
-﻿namespace PerformanceStubs {
-    using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
+namespace PerformanceStubs {
     public class PerformanceTestCandidateResult {
         public string Title { get; set; }
         public List<long> ElapsedTicksCollection { get; set; }
+        public double Average {
+            get {
+                return ElapsedTicksCollection != null ? ElapsedTicksCollection.Average() : 0;
+            }
+        }
+        public double GetRatioOfWorst(double worstAverage) {
+            return worstAverage / Average;
+        }
 
         public PerformanceTestCandidateResult(string title, List<long> elapsedTicksCollection) {
             this.Title = title;
--- PerformanceStubs/Core/PerformanceTestSummary.cs
+++ PerformanceStubs/Core/PerformanceTestSummary.cs
@@ -1,11 +1,17 @@
-﻿namespace PerformanceStubs {
-    using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
+namespace PerformanceStubs {
     public class PerformanceTestSummary {
         public string Title { get; set; }
         public string Caption { get; set; }
         public List<PerformanceTestCandidateResult> Results { get; set; }
         public long Iterations { get; set; }
+        public double WorstAverage {
+            get {
+                return Results != null ? Results.Max(result => result.ElapsedTicksCollection.Average()) : 0;
+            }
+        }
 
         public PerformanceTestSummary(string title, string caption, List<PerformanceTestCandidateResult> results, long iterations) {
             this.Title = title;
